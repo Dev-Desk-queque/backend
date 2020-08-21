7823ffd0ff476150c490c9efbfe4c60e95a44652
Devin Bidwell <dbidwell@intuituscyber.com>
2020-08-21 16:23:55 -0700

    boilerplate

---
+++ build/precache-manifest.1bc44f64a7bc75a777c20b3dc1763017.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1bfa1ae437167a284568a6fd8828b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43ee447342e0d6a1b97",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5b829104f5ab72bdbe0",
+    "url": "/static/js/2.0d9955e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d9955e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43ee447342e0d6a1b97",
+    "url": "/static/js/main.58840a07.chunk.js"
+  },
+  {
+    "revision": "96dd2360289c6d5dfb5d",
+    "url": "/static/js/runtime-main.1eacd3ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
