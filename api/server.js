1f2d7272930d1368b1f3ac606b6e7a5c20771feb
Dudeicle <brian-griffiths@lambdastudents.com>
2020-08-24 15:03:38 -0400

    added cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const DevDeskRouter = require("../devdesk/dev-router");
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use("/api/devdesk", DevDeskRouter);
 
